feb0ceb0deee5b8415d38dda5155ddfa2e0e02c8
Richard O. Gregory <richardgrecoson@gmail.com>
2019-01-07 10:31:23 +0100

    Make application.py code comply with pep8 styleguide

--- application.py
+++ application.py
@@ -12,83 +12,64 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-# Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'},
-                {'name':'Blue Burgers', 'id':'2'},
-                {'name':'Taco Hut', 'id':'3'}]
-
-
-# Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese',
-        'price':'$5.99','course' :'Entree', 'id':'1'},
-        {'name':'Chocolate Cake','description':'made with Dutch Chocolate',
-        'price':'$3.99', 'course':'Dessert','id':'2'},
-        {'name':'Caesar Salad',
-        'description':'with fresh organic vegetables','price':'$5.99',
-        'course':'Entree','id':'3'},
-        {'name':'Iced Tea',
-        'description':'with lemon','price':'$.99', 'course':'Beverage',
-        'id':'4'},
-        {'name':'Spinach Dip', 'description':'creamy dip with fresh spinach',
-        'price':'$1.99', 'course':'Appetizer','id':'5'} ]
-
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese',
-        'price':'$5.99','course' :'Entree'}
 
 @app.route('/')
 @app.route('/categories')
 def showCategories():
     categories = session.query(Category).all()
-    return render_template ('categories.html', cat=categories)
+    return render_template('categories.html', cat=categories)
 
 
 @app.route('/category/<int:category_id>/')
 @app.route('/category/<int:category_id>/item')
 def showCategoryItems(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    category_items = session.query(CategoryItem).filter_by(category_id=category_id)
-    return render_template ('categoryitem.html', items=category_items,
-                            category_id=category_id, category=category)
+    i = session.query(CategoryItem).filter_by(category_id=category_id)
+    category_items = i
+    return render_template('categoryitem.html', items=category_items,
+                           category_id=category_id, category=category)
 
 
 @app.route('/category/<int:category_id>/item/new',
            methods=['GET', 'POST'])
 def newCategoryItem(category_id):
     if request.method == 'POST':
-        new_category_item = CategoryItem(title=request.form['title'],
-                                        description=request.form['description'],
-                                        author=request.form['author'],
-                                        category_id=category_id)
-        session.add(new_category_item)
+        new_item = CategoryItem(title=request.form['title'],
+                                description=request.form['description'],
+                                author=request.form['author'],
+                                category_id=category_id)
+        session.add(new_item)
         session.commit()
         return redirect(url_for('showCategories', category_id=category_id))
     else:
-        return render_template ('newcategoryitem.html', category_id=category_id)
+        return render_template('newcategoryitem.html',
+                               category_id=category_id)
 
 
 @app.route('/category/<int:category_id>/item/<int:category_item_id>')
 def categoryItemDescription(category_id, category_item_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    category_item = session.query(CategoryItem).filter_by(category_id=category_id).filter_by(id=category_item_id)
-    return render_template ('categoryiteminfo.html', category_id=category_id,
-                            category_item_id=category_item_id, cat_item=category_item,
-                            category=category)
+    i = session.query(CategoryItem).filter_by(category_id=category_id)
+    category_item = i.filter_by(id=category_item_id)
+
+    return render_template('categoryiteminfo.html', category_id=category_id,
+                           category_item_id=category_item_id,
+                           cat_item=category_item,
+                           category=category)
 
 
 @app.route('/category/<int:category_id>/item/<int:category_item_id>/edit',
            methods=['GET', 'POST'])
 def editCategoryItem(category_id, category_item_id):
-    return render_template ('editcategoryitem.html', category_id=category_id,
-                            category_item_id=category_item_id)
+    return render_template('editcategoryitem.html', category_id=category_id,
+                           category_item_id=category_item_id)
 
 
 @app.route('/category/<int:category_id>/item/<int:category_item_id>/delete',
            methods=['GET', 'POST'])
 def deleteCategoryItem(category_id, category_item_id):
-    return render_template ('deletecategoryitem.html', category_id=category_id,
-                            category_item_id=category_item_id)
+    return render_template('deletecategoryitem.html', category_id=category_id,
+                           category_item_id=category_item_id)
 
 
 if __name__ == '__main__':
