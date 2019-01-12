aba70df29652f73be3b27474f263c0cb75124d22
Richard O. Gregory <richardgrecoson@gmail.com>
2019-01-12 09:12:22 +0100

    Add a working google 'gdisconnect' method.
    This method deletes the seesion of logged in user. That then disconnects them from the app

--- application.py
+++ application.py
@@ -129,6 +129,38 @@ def gconnect():
     return output
 
 
+# DISCONNECT - Revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print ('Access Token is None')
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print ('In gdisconnect access token is %s', access_token)
+    print ('User name is: ')
+    print (login_session['username'])
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print ('result is ')
+    print (result)
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 @app.route('/category/<int:category_id>/item/JSON')
 def categoryItemJSON(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
@@ -145,7 +177,7 @@ def showCategories():
 
 
 @app.route('/category/<int:category_id>/')
-@app.route('/category/<int:category_id>/item')
+@app.route('/category/<int:category_id>/item/')
 def showCategoryItems(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     i = session.query(CategoryItem).filter_by(category_id=category_id)
@@ -154,7 +186,7 @@ def showCategoryItems(category_id):
                            category_id=category_id, category=category)
 
 
-@app.route('/category/<int:category_id>/item/new',
+@app.route('/category/<int:category_id>/item/new/',
            methods=['GET', 'POST'])
 def newCategoryItem(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
@@ -172,7 +204,7 @@ def newCategoryItem(category_id):
                                category_id=category_id, category=category)
 
 
-@app.route('/category/<int:category_id>/item/<int:category_item_id>')
+@app.route('/category/<int:category_id>/item/<int:category_item_id>/')
 def categoryItemDescription(category_id, category_item_id):
     category = session.query(Category).filter_by(id=category_id).one()
     i = session.query(CategoryItem).filter_by(category_id=category_id)
